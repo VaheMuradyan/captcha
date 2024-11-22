3611cfceda74b47989772ca1983168a37eea2b65
VaheMuradyan <vahenmuradyan02@outlook.com>
2024-11-22 14:36:39 +0400

    back to work

--- captcha-project/src/App.vue
+++ captcha-project/src/App.vue
@@ -37,7 +37,6 @@ export default {
       }
     },
 
-
     async verifyCaptcha() {
     try {
     // Send verification request to the backend
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log"
+	"net/http"
 	"time"
 
 	"github.com/dchest/captcha"
@@ -18,7 +19,6 @@ func main() {
 	initRedis()
 
 	redisStore := NewRedisStore(rdb, time.Minute*5, ctx)
-	captchaController := NewCaptchaController(&redisStore)
 	captcha.SetCustomStore(redisStore)
 
 	r := gin.Default()
@@ -31,11 +31,61 @@ func main() {
 		AllowCredentials: true,
 	}))
 
-	r.GET("/captcha", captchaController.CreateCaptcha)
+	r.GET("/captcha", func(c *gin.Context) {
+		captchaId := captcha.New()
 
-	r.GET("/captcha/image/:id", captchaController.RenderCaptchaImage)
+		captchaDigiths := captcha.RandomDigits(6)
 
-	r.POST("/captcha/verify", captchaController.VerifyCaptcha)
+		redisStore.Set(captchaId, captchaDigiths)
+
+		c.IndentedJSON(http.StatusOK, gin.H{"id": captchaId})
+	})
+
+	r.GET("/captcha/image/:id", func(c *gin.Context) {
+		captchaId := c.Param("id")
+		log.Printf("Requested CAPTCHA ID: %s\n", captchaId)
+
+		digits := redisStore.Get(captchaId, false)
+		if digits == nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "captcha not found"})
+			return
+		}
+
+		c.Header("Content-Type", "image/png")
+		if err := captcha.WriteImage(c.Writer, captchaId, captcha.StdWidth, captcha.StdHeight); err != nil {
+			log.Printf("Failed to render CAPTCHA: %v\n", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to render captcha"})
+		}
+	})
+
+	r.POST("/captcha/verify", func(c *gin.Context) {
+
+		var req struct {
+			Id     string `json:"id"`
+			Answer string `json:"answer"`
+		}
+
+		if err := c.ShouldBindJSON(&req); err != nil {
+			c.IndentedJSON(http.StatusBadRequest, gin.H{"error": "invalid input"})
+			return
+		}
+
+		storedCode, err := rdb.Get(ctx, req.Id).Result()
+		if err == redis.Nil {
+			c.IndentedJSON(http.StatusNotFound, gin.H{"error": "captcha not found or expired"})
+			return
+		} else if err != nil {
+			c.IndentedJSON(http.StatusInternalServerError, gin.H{"error": "redis error"})
+			return
+		}
+
+		if storedCode == req.Answer {
+			rdb.Del(ctx, req.Id)
+			c.IndentedJSON(http.StatusOK, gin.H{"status": "success"})
+		} else {
+			c.IndentedJSON(http.StatusUnauthorized, gin.H{"status": "failure"})
+		}
+	})
 
 	r.Run(":8080")
 }
